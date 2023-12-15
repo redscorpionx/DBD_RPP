bb351ff5f7073c0f7b8ef42bf584076ea0bb695b
RedScorpion <38502084+redscorpionx@users.noreply.github.com>
2023-12-15 22:06:13 +0300

    Refactored random_picks.py to use config file for perk coordinates and added functions to select perks.

--- src/random_picks.py
+++ src/random_picks.py
@@ -1,62 +1,51 @@
+import json
 import random
+import time
 
-# Assuming perks are stored in a list, where each page is a sublist
-all_perks = [
-    [
-        "Perk1",
-        "Perk2",
-        "Perk3",
-        "Perk4",
-        "Perk5",
-        "Perk6",
-        "Perk7",
-        "Perk8",
-        "Perk9",
-        "Perk10",
-        "Perk11",
-        "Perk12",
-        "Perk13",
-        "Perk14",
-        "Perk15",
-    ],
-    [
-        "Perk16",
-        "Perk17",
-        "Perk18",
-        "Perk19",
-        "Perk20",
-        "Perk21",
-        "Perk22",
-        "Perk23",
-        "Perk24",
-        "Perk25",
-        "Perk26",
-        "Perk27",
-        "Perk28",
-        "Perk29",
-        "Perk30",
-    ],
-]
+import pyautogui
+
+# Load config from config.json
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
 
 
-def pick_random_perks():
-    selected_perks = set()
-
-    while len(selected_perks) < 4:
-        # Choose a random page
-        random_page = random.choice(all_perks)
-
-        # Choose a random perk from the selected page
-        random_perk = random.choice(random_page)
-
-        # Add the perk to the set of selected perks
-        selected_perks.add(random_perk)
-
-    return list(selected_perks)
+# Function to click on a given coordinate
+def click_coordinate(x, y):
+    pyautogui.click(x, y)
+    time.sleep(1)  # Wait for the action to complete
 
 
-# Get the randomly selected perks
-selected_perks = pick_random_perks()
+# Function to select a random perk from a given row
+def select_random_perk(row):
+    perk_list = list(config["perkCoordinates"][row].keys())
+    random_perk = random.choice(perk_list)
+    return config["perkCoordinates"][row][random_perk]
 
-# Display the result
-print(selected_perks)
+
+# Function to select a random page
+def select_random_page():
+    page_list = list(config["pages_coordinates"].keys())
+    random_page = random.choice(page_list)
+    return config["pages_coordinates"][random_page]
+
+
+# Function to select perks for all four slots
+def select_perks():
+    for i in range(1, 5):
+        # Select perk slot
+        slot = f"slot_{i}"
+        slot_coordinates = config["perkSlotsCoordinates"][slot]
+        click_coordinate(slot_coordinates["x"], slot_coordinates["y"])
+
+        # Select random page
+        page_coordinates = select_random_page()
+        click_coordinate(page_coordinates["x"], page_coordinates["y"])
+
+        # Select random perk from each row
+        for row in config["perkCoordinates"]:
+            perk_coordinates = select_random_perk(row)
+            click_coordinate(perk_coordinates["x"], perk_coordinates["y"])
+
+
+# Run the script
+select_perks()
