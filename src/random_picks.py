7734dd829181575f4b9490a609ff331b03f87e8d
RedScorpion <38502084+redscorpionx@users.noreply.github.com>
2023-12-15 21:19:26 +0300

    Refactor perk coordinates and remove duplicate code

--- src/random_picks.py
+++ src/random_picks.py
@@ -61,8 +61,41 @@ selected_perks = pick_random_perks()
 # Display the result
 print(selected_perks)
 
-# pages one to nine coordinates and the center page
-center_five = (665, 930)
+
+# screen size: 1920 x 1080
+
+# Perk slots coordinates
+perk_slot_one = (495, 930)
+perk_slot_two = (540, 930)
+perk_slot_three = (581, 930)
+perk_slot_four = (622, 930)
+
+
+# # perk coordinates on each page
+
+# # perks 1 - 5 (row 1)
+perk_one = (392, 630)
+perk_two = (515, 630)
+perk_three = (643, 630)
+perk_four = (764, 630)
+perk_five = (885, 630)
+
+# # perks 6 - 10 (row 2)
+perk_six = (460, 722)
+perk_seven = (581, 722)
+perk_eight = (711, 722)
+perk_nine = (826, 722)
+perk_ten = (958, 722)
+
+# # perks 11 - 15 (row 3)
+perk_eleven = (395, 814)
+perk_twelve = (517, 814)
+perk_thirteen = (645, 814)
+perk_fourteen = (771, 814)
+perk_fifteen = (887, 814)
+
+
+# pages coordinates
 one = (495, 930)
 two = (540, 930)
 three = (581, 930)
@@ -72,9 +105,3 @@ six = (705, 930)
 seven = (746, 930)
 eight = (790, 930)
 nine = (832, 930)
-
-# Perk slots coordinates
-perk_slot_one = (495, 930)
-perk_slot_two = (540, 930)
-perk_slot_three = (581, 930)
-perk_slot_four = (622, 930)
