8f3e32fbfe8f5d56a7320c19168f09fa8eadf496
RedScorpion <38502084+redscorpionx@users.noreply.github.com>
2023-12-16 02:06:06 +0300

    version 0.0.1

--- src/random_picks.py
+++ src/random_picks.py
@@ -1,23 +1,35 @@
+# TODO:
+# + Impelement a gui interface for user-friendliness.
+# + Impelement an overlay to hide perks picked till game starts.
+# + Impelement perk exception selection so players can choose which perks to exclude from the random selection.
+
+# FIXME:
+# + Fix the perk selection so it doesn't pick the same perk twice no matter what.
+# + Efficiency/Speed/Maintainability improvements.
+
+
 import json
 import random
 import time
 
 import pyautogui
 
+time.sleep(5)  # Add a delay to let the user go to the game window
+
 
 def move_mouse(x, y):
     pyautogui.moveTo(x, y)
-    time.sleep(1)  # Add a delay to ensure actions are registered
+    time.sleep(0.5)
 
 
 def click_coordinates(x, y):
     pyautogui.click(x, y)
-    # time.sleep(1)  # Add a delay to ensure actions are registered
+    # time.sleep(1)
 
 
 def double_click_coordinates(x, y):
-    pyautogui.doubleClick(x, y, 1)
-    time.sleep(1)  # Add a delay to ensure actions are registered
+    pyautogui.doubleClick(x, y, 0.5)
+    time.sleep(1)
 
 
 def main():
@@ -29,13 +41,13 @@ def main():
     pages_coordinates = config["pages_coordinates"]
 
     for slot, slot_coordinates in perk_slots_coordinates.items():
-        print(f"* Processing {slot}...")
+        print(f"-----Processing {slot}-----")
 
         # Click on the slot
         move_mouse(slot_coordinates["x"], slot_coordinates["y"])
         click_coordinates(slot_coordinates["x"], slot_coordinates["y"])
 
-        print(f"* Selected slot: {slot} at {slot_coordinates}")
+        print(f"* Selected {slot}")
 
         # Double-click on page_5 coordinate
         move_mouse(pages_coordinates["page_5"]["x"], pages_coordinates["page_5"]["y"])
@@ -43,23 +55,29 @@ def main():
             pages_coordinates["page_5"]["x"], pages_coordinates["page_5"]["y"]
         )
 
-        print(f"* Selected center page: {pages_coordinates['page_5']}")
+        print("* Selected center page: page_5")
 
         # Click on a randomly chosen page
         random_page = random.choice(list(pages_coordinates.values()))
         move_mouse(random_page["x"], random_page["y"])
-        pyautogui.click(random_page["x"], random_page["y"], 2, 1)
+        pyautogui.click(random_page["x"], random_page["y"], 2, 0.5)
 
-        print(f"* Selected random page: {random_page}")
+        print(
+            f"* Selected random page: {list(pages_coordinates.keys())[list(pages_coordinates.values()).index(random_page)]}"
+        )
 
         # Select a random row
         random_row = random.choice(list(perk_coordinates.values()))
-        print(f"* Choosen random row: {random_row}")
+        print(
+            f"* Selected random row: {list(perk_coordinates.keys())[list(perk_coordinates.values()).index(random_row)]}"
+        )
 
         # Click on a randomly chosen perk within the selected row
         random_perk = random.choice(list(random_row.values()))
         move_mouse(random_perk["x"], random_perk["y"])
-        print(f"* Selected random perk: {random_perk}")
+        print(
+            f"* Selected random perk: {list(random_row.keys())[list(random_row.values()).index(random_perk)]}"
+        )
 
         click_coordinates(random_perk["x"], random_perk["y"])
         print("-----------")
